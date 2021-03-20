e5f961f66150febe3e75921d22f3d4af043c97aa
szakmaster <gszakacs@outlook.com>
2021-03-20 17:02:34 +0000

    Added new Blog project.

---
+++ Blog/Blog/Data/BlogPostsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogPostsContext : DbContext
+    {
+        public BlogPostsContext (DbContextOptions<BlogPostsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blog.Models.BlogPost> BlogPost { get; set; }
+    }
+}
