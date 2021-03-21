03fe75ae3dc1fec284530c7349013c7e39e03bdb
szakmaster <gszakacs@outlook.com>
2021-03-21 14:48:41 +0000

    Added basic routing to ClientApp.

--- Blog/Blog/ClientApp/src/app/app-routing.module.ts
+++ Blog/Blog/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogPostAddEditComponent } from './blog-post-add-edit/blog-post-add-edit.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { BlogPostsComponent } from './blog-posts/blog-posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BlogPostsComponent, pathMatch: 'full' },
+  { path: 'blogpost/:id', component: BlogPostComponent },
+  { path: 'add', component: BlogPostAddEditComponent },
+  { path: 'blogpost/edit/:id', component: BlogPostAddEditComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
