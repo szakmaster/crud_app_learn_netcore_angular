6ae3f9b616676dfc2c840e1c492c7cadbca180a7
szakmaster <gszakacs@outlook.com>
2021-04-12 23:21:44 +0200

    Added new components to the client.

--- ClientApp/src/app/blog-posts/blog-posts.component.ts
+++ ClientApp/src/app/blog-posts/blog-posts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogPostService } from '../services/blog-post.service';
+import { BlogPost } from '../models/blogpost';
 
 @Component({
   selector: 'app-blog-posts',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-posts.component.scss']
 })
 export class BlogPostsComponent implements OnInit {
+  blogPosts$!: Observable<BlogPost[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private blogPostService: BlogPostService) {
   }
 
+  ngOnInit() {
+    this.loadBlogPosts();
+  }
+
+  loadBlogPosts() {
+    this.blogPosts$ = this.blogPostService.getBlogPosts();
+  }
+
+  delete(postId?: number) {
+    const ans = confirm('Do you want to delete blog post with id: ' + postId);
+    if (ans) {
+      this.blogPostService.deleteBlogPost(postId).subscribe((data) => {
+        this.loadBlogPosts();
+      });
+    }
+  }
 }
